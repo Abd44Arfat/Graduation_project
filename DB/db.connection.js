af40624ded248deec798dc72c755bc02df7cb88d
abdelrahman arfat <abdoarfat924@gmail.com>
2024-09-28 11:02:45 +0300

    ..

--- DB/db.connection.js
+++ DB/db.connection.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose'
 
-export const dbConnection =mongoose.connect('mongodb://localhost:27017/project1').then(()=>{
+export const dbConnection =mongoose.connect('mongodb+srv://abdo:06925270@cluster0.6line1m.mongodb.net/').then(()=>{
     console.log("Database Connected Successfully ..");
-}).catch(()=>{
-    console.log("Error in Connection DB ..");
+}).catch((e)=>{
+    console.log(`Error in Connection DB ..${e}`);
     
 })
--- src/modules/user/user.validation.js
+++ src/modules/user/user.validation.js
@@ -3,7 +3,7 @@ import joi from 'joi'
 const signupVal= joi.object({
     name:joi.string().min(2).max(30).required(),
     email:joi.string().email().required(),
-    password:joi.string().pattern(/^[A-Z][A-Za-z0-9]{8,40}$/).required(),
+    password:joi.string().pattern(/^[A-Z][A-Za-z0-9]{7,40}$/).required(),
     repassword:joi.valid(joi.ref('password')).required()
 })
 
