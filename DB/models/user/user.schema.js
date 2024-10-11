52a11253f34772a1a4f3ebcb6efdd6b5f058c624
Mostafaamin1000 <170482014+Mostafaamin1000@users.noreply.github.com>
2024-10-11 06:50:20 -0700

    Your commit message

--- DB/models/user/user.schema.js
+++ DB/models/user/user.schema.js
@@ -25,7 +25,7 @@ this.password =bcrypt.hashSync(this.password , 8)
 })
 
 schema.pre('findOneAndUpdate',function(){
-    this._update.password =bcrypt.hashSync(this._update.password , 8)
+    if(this._update.password)  this._update.password =bcrypt.hashSync(this._update.password , 8)
     })
 
 export const User =model('User',schema)
