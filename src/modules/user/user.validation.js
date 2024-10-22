79f1123f92041453849c696385728a9010dff023
Mostafaamin1000 <170482014+Mostafaamin1000@users.noreply.github.com>
2024-10-21 18:01:13 -0700

    your commit msg

--- src/modules/user/user.validation.js
+++ src/modules/user/user.validation.js
@@ -3,7 +3,7 @@ import joi from 'joi'
 const signupVal= joi.object({
     name:joi.string().min(2).max(30).required(),
     email:joi.string().email().required(),
-    password:joi.string().min(7).max(40).pattern(/^[^A-Z][A-Za-z0-9]{7,39}$/).required(),
+    password:joi.string().min(7).max(40).pattern(/^[A-Za-z][A-Za-z0-9]{7,39}$/).required(),
     repassword:joi.valid(joi.ref('password')).required(),
     role:joi.string().required()
 })
@@ -12,8 +12,8 @@ const signupVal= joi.object({
 
 const signinVal= joi.object({
     email:joi.string().email().required(),
-    password:joi.string().min(7).max(40).pattern(/^[^A-Z][A-Za-z0-9]{7,39}$/).required().messages({
-        'string.pattern.base': 'Password must start with an uppercase letter and be between 8-40 characters long',
+    password:joi.string().min(7).max(40).pattern(/^[A-Za-z][A-Za-z0-9]{7,39}$/).required().messages({
+        'string.pattern.base': 'Password must be between 8-40 characters long',
         'string.min': 'Password must be at least 8 characters long',
         'string.max': 'Password must be no longer than 40 characters',
         'string.empty': 'Password is required'})
@@ -23,13 +23,13 @@ const signinVal= joi.object({
 
 const changePasswordVal= joi.object({
     email:joi.string().email().required(),
-    oldPassword:joi.string().min(7).max(40).pattern(/^[^A-Z][A-Za-z0-9]{7,39}$/).required().messages({
-        'string.pattern.base': 'oldPassword must start with an uppercase letter and be between 8-40 characters long',
+    oldPassword:joi.string().min(7).max(40).pattern(/^[A-Za-z][A-Za-z0-9]{7,39}$/).required().messages({
+        'string.pattern.base': 'oldPassword must be between 8-40 characters long',
         'string.min': 'oldPassword must be at least 8 characters long',
         'string.max': 'oldPassword must be no longer than 40 characters',
         'string.empty': 'oldPassword is required'}),
-        newPassword:joi.string().min(7).max(40).pattern(/^[^A-Z][A-Za-z0-9]{7,39}$/).required().messages({
-            'string.pattern.base': 'newPassword must start with an uppercase letter and be between 8-40 characters long',
+        newPassword:joi.string().min(7).max(40).pattern(/^[A-Za-z][A-Za-z0-9]{7,39}$/).required().messages({
+            'string.pattern.base': 'newPassword must be between 8-40 characters long',
             'string.min': 'newPassword must be at least 8 characters long',
             'string.max': 'newPassword must be no longer than 40 characters',
             'string.empty': 'newPassword is required'})
