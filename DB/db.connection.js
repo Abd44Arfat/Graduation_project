2e642c264e498455aa877ca159069a78f02dde9b
abdelrahman arfat <102757681+Abd44Arfat@users.noreply.github.com>
2024-12-04 07:10:38 +0200

    Update db.connection.js

--- DB/db.connection.js
+++ DB/db.connection.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-export const dbConnection =mongoose.connect('mongodb://localhost:27017/project1').then(()=>{
+export const dbConnection =mongoose.connect('mongodb+srv://abdelrahman:06925270@cluster0.6line1m.mongodb.net/').then(()=>{
     console.log("Database Connected Successfully ..");
 }).catch(()=>{
     console.log("Error in Connection DB ..");
